fa09446665d2f2826aac845dbbae8b2f9e760346
wh <whan6795@foxmail.com>
2021-05-04 00:37:20 +0800

    change host into 0.0.0.0

--- app.py
+++ app.py
@@ -25,4 +25,4 @@ def create_app():
 
 if __name__ == '__main__':
     app = create_app()
-    app.run(debug=True, host='127.0.0.1', port='8080')
+    app.run(debug=True, host='0.0.0.0', port='8080')
