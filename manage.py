35233b24fbec3daeefb86f6ddf0b283da3629799
wh <whan6795@foxmail.com>
2021-05-04 01:42:41 +0800

    fix bug

--- manage.py
+++ manage.py
@@ -2,7 +2,8 @@
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from app import create_app
-from exts import db,make_password
+from exts import db
+from utils.login import make_password
 from apps.admin import models as admin_models
 
 app = create_app()
