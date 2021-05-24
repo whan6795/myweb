6e08a14b0f061e46c27fab6a87a4bc6f174abfdc
wh <whan6795@foxmail.com>
2021-05-24 19:53:23 +0800

    continue...

--- conf/guni.py
+++ conf/guni.py
@@ -2,12 +2,10 @@
 from gevent import monkey
 
 monkey.patch_all()
-import multiprocessing
-
 debug = True
 loglevel = 'debug'
 bind = '0.0.0.0:8080'
 pidfile = 'log/gunicorn.pid'
 logfile = 'log/debug.log'
-workers = multiprocessing.cpu_cout() * 2 + 1
+workers = 3
 worker_class = 'gevent'
