dc5cbae0f446e5986bea74656c44845936b2147b
wh <whan6795@foxmail.com>
2021-05-24 20:20:21 +0800

    continue...

--- app.py
+++ app.py
@@ -9,15 +9,14 @@ from flask_wtf import CSRFProtect
 from datetime import datetime
 
 
-def create_app():
-    app = Flask(__name__)
-    app.register_blueprint(admin_bp)
-    app.register_blueprint(common_bp)
-    app.register_blueprint(front_bp)
-    app.config.from_object('config')
-    CSRFProtect(app)
-    db.init_app(app)
-    return app
+app = Flask(__name__)
+app.register_blueprint(admin_bp)
+app.register_blueprint(common_bp)
+app.register_blueprint(front_bp)
+app.config.from_object('config')
+CSRFProtect(app)
+db.init_app(app)
+
 
 
 # @app.route('/')
@@ -26,7 +25,6 @@ def create_app():
 
 
 if __name__ == '__main__':
-    app = create_app()
     with open('utils/start.txt', 'w') as f:
         f.write(str(datetime.timestamp(datetime.now())))
     app.run(debug=True)  # , host='0.0.0.0', port='8080'
